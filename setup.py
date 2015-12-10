5aa30fe9dbc96c9e7ad1d748093f578228912a52
John-Paul Stanford <dev@stanwood.org.uk>
2015-12-10 09:35:53 +0000

    Moved things arround

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
-from setuptools import setup
+from distutils.core import setup
+import os
+import sys
+from setuptools import find_packages, setup
+
+os.chdir(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'src'))
+
+EXCLUDE_FROM_PACKAGES = []
+
 setup(
-    name='myextension',
+    name='markdown-inclusion',
+    description='This is plugin to extend python markdown and allow it include other markdown files.'
+    author='John-Paul Stanford',
+    author_email='John-Paul.Stanford@arm.com',
+    url='https://github.com/shield007/markdown-inclusion',
+    packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    include_package_data=True,
     version='1.0',
     py_modules=['myextension'],
     install_requires = ['markdown>=2.5'],
